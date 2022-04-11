46521d191393c6cb984b968f894defeee7bf5631
dgamarra <dgamarra@continental.edu.pe>
2022-04-11 16:50:05 -0500

    Se implementó y probó la operación resta

--- src/logica/Suma.py
+++ src/logica/Suma.py
@@ -1,3 +1,6 @@
 class Suma:
     def operacionSuma(self,sumando1,sumando2):
         return sumando1+sumando2
+
+    def operacionResta(self,minuendo,sustraendo):
+        return minuendo-sustraendo
--- tests/PruebaSuma.py
+++ tests/PruebaSuma.py
@@ -10,24 +10,36 @@ class PruebaSuma(unittest.TestCase):
 
     def test_operacionSuma_dosNumerosPositivos_retornaSuma(self):
         # Arrange
-        self.sumando1=19
-        self.sumando2=50
+        self.minuendo=19
+        self.sustraendo=50
         self.resultadoesperado=69
 
         # Do
-        self.resultadoActual=self.suma.operacionSuma(self.sumando1,self.sumando2)
+        self.resultadoActual=self.suma.operacionSuma(self.minuendo, self.sustraendo)
 
         # Assert
         self.assertEqual(self.resultadoesperado,self.resultadoActual)
 
     def test_operacionSuma_dosNumerosNegativos_retornaSuma(self):
         # Arrange
-        self.sumando1=-3
-        self.sumando2=-7
+        self.minuendo=-3
+        self.sustraendo=-7
         self.resultadoesperado=-10
 
         # Do
-        self.resultadoActual=self.suma.operacionSuma(self.sumando1,self.sumando2)
+        self.resultadoActual=self.suma.operacionSuma(self.minuendo, self.sustraendo)
+
+        # Assert
+        self.assertEqual(self.resultadoesperado,self.resultadoActual)
+
+    def test_operacionResta_dosNumerosPositivos_retornaResta(self):
+        # Arrange
+        self.minuendo=50
+        self.sustraendo=30
+        self.resultadoesperado=20
+
+        # Do
+        self.resultadoActual=self.suma.operacionResta(self.minuendo, self.sustraendo)
 
         # Assert
         self.assertEqual(self.resultadoesperado,self.resultadoActual)
